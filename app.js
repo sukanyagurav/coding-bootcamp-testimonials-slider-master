9628e8033ec94e8748adb6541602fc2919b7dd86
Sukanya Gurav <sukanyagurav6@gmail.com>
2023-05-25 22:25:54 +0530

    file updated

--- app.js
+++ app.js
@@ -3,14 +3,14 @@ const data=[
         id:1,
         img:'images/image-tanya.jpg',
         name:'Tanya Sinclair',
-        description:'\"I’ve been interested in coding for a while but never taken the jump, until now. I couldn’t recommend this course enough. I’m now in the job of my dreams and so  excited about the future.\"',
+        description:" \" I’ve been interested in coding for a while but never taken the jump, until now. I couldn’t recommend this course enough. I’m now in the job of my dreams and so  excited about the future. \"",
         profession:'UX Engineer'
     },
     {
         id:2,
         img:'images/image-john.jpg',
         name:'John Tarkpor',
-        description:'\"If you want to lay the best foundation possible I’d recommend taking this course. The depth the instructors go into is incredible. I now feel so confident about starting up as a professional developer.\"',
+        description:' \" If you want to lay the best foundation possible I’d recommend taking this course. The depth the instructors go into is incredible. I now feel so confident about starting up as a professional developer. \"',
         profession:'Junior Front-end Developer'
     }
 ]
